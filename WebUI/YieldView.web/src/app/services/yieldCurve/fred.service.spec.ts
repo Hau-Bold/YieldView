3f9e8bfa1a4dcdc287549ab5c79982f1dfd075db
hau-bold <michael-krasser@t-online.de>
2025-10-22 19:24:08 +0200

    [FE] added missing tests for FredService

--- WebUI/YieldView.web/src/app/services/yieldCurve/fred.service.spec.ts
+++ WebUI/YieldView.web/src/app/services/yieldCurve/fred.service.spec.ts
@@ -3,6 +3,8 @@ import { HttpTestingController, provideHttpClientTesting } from '@angular/common
 import { FredService } from './fred.service';
 import { GPSPrice } from '../../Modules/GPSPrice';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { Wilshire5000Price } from '../../Modules/Wilshire5000Price';
+import { BuffetIndicator } from '../../Modules/BuffetIndicator';
 
 describe('FredService', () => {
   let service: FredService;
@@ -28,6 +30,7 @@ describe('FredService', () => {
     expect(service).toBeTruthy();
   });
 
+describe('getGDPPrices', () => {
   it('should call the correct URL with given parameters and return expected data', () => {
     const mockData: GPSPrice[] = [
       { date: '2025-01-01', value: 123.45 },
@@ -63,4 +66,81 @@ describe('FredService', () => {
 
     req.flush([]); 
   });
+});
+
+describe('getW5000Prices', () => {
+  it('should call the correct URL with given parameters and return expected data', () => {
+    const mockData: Wilshire5000Price[] = [
+      { date: '2025-01-01', value: 123.45 ,open: 120.00, high: 125.00, low: 119.00},
+      { date: '2025-02-01', value: 234.56,open: 121.00, high: 250.00, low: 120.00 }
+    ];
+
+    const from = '2025-01-01';
+    const to = '2025-02-01';
+
+    service.getW5000Prices(from, to).subscribe((data) => {
+      expect(data).toEqual(mockData);
+    });
+
+    const req = httpMock.expectOne(
+      `https://localhost:7031/api/fred/w5000?from=${from}&to=${to}`
+    );
+
+    expect(req.request.method).toBe('GET');
+    req.flush(mockData); 
+  });
+
+  it('should handle empty responses gracefully', () => {
+    const from = '2025-01-01';
+    const to = '2025-02-01';
+
+    service.getW5000Prices(from, to).subscribe((data) => {
+      expect(data.length).toBe(0);
+    });
+
+    const req = httpMock.expectOne(
+      `https://localhost:7031/api/fred/w5000?from=${from}&to=${to}`
+    );
+
+    req.flush([]); 
+  });
+});
+
+describe('getBuffetIndicatorPrices', () => {
+  it('should call the correct URL with given parameters and return expected data', () => {
+    const mockData: BuffetIndicator[] = [
+      { date: '2025-01-01', value: 123.45},
+      { date: '2025-02-01', value: 234.56}
+    ];
+
+    const from = '2025-01-01';
+    const to = '2025-02-01';
+
+    service.getBuffetIndicatorPrices(from, to).subscribe((data) => {
+      expect(data).toEqual(mockData);
+    });
+
+    const req = httpMock.expectOne(
+      `https://localhost:7031/api/fred/buffett-indicator?from=${from}&to=${to}`
+    );
+
+    expect(req.request.method).toBe('GET');
+    req.flush(mockData); 
+  });
+
+  it('should handle empty responses gracefully', () => {
+    const from = '2025-01-01';
+    const to = '2025-02-01';
+
+    service.getBuffetIndicatorPrices(from, to).subscribe((data) => {
+      expect(data.length).toBe(0);
+    });
+
+    const req = httpMock.expectOne(
+      `https://localhost:7031/api/fred/buffett-indicator?from=${from}&to=${to}`
+    );
+
+    req.flush([]); 
+  });
+});
 });
