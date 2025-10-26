8db283c30cf5f88f27a3a21d01788bf1bde200f3
hau-bold <michael-krasser@t-online.de>
2025-10-26 15:05:48 +0100

    added some tests for FredDataProvider

--- YieldView.Test.Int/Impl/Providers/FREDDataProviderTests.cs
+++ YieldView.Test.Int/Impl/Providers/FREDDataProviderTests.cs
@@ -12,19 +12,31 @@ public class FREDDataProviderTests
   private IServiceScopeFactory scopeFactory = null!;
   private FREDDataProvider sut = null!;
 
+  private readonly List<WilshirePrice> wilshirePrices =
+  [
+      new() { Date = new DateTime(2020, 1, 1), Value = 24000 },
+      new() { Date = new DateTime(2021, 1, 1), Value = 27000 },
+      new() { Date = new DateTime(2022, 1, 1), Value = 31000 }
+  ];
+
+  private readonly List<GDPPrice> gdpPrices =
+  [
+      new() { Date = new DateTime(2020, 1, 1), Value = 20000 },
+      new() { Date = new DateTime(2021, 1, 1), Value = 22000 },
+      new() { Date = new DateTime(2022, 1, 1), Value = 25000 }
+  ];
+
   [SetUp]
   public void SetUp()
   {
     var services = new ServiceCollection();
 
     services.AddDbContext<YieldDbContext>(options =>
-        options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+        options.UseInMemoryDatabase("testdb"));
 
     serviceProvider = services.BuildServiceProvider();
     scopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();
     sut = new FREDDataProvider(scopeFactory);
-
-    SeedTestData();
   }
 
   [TearDown]
@@ -35,11 +47,15 @@ public class FREDDataProviderTests
   [Test]
   public async Task GetGDPPricesAsync_FiltersByDateRange()
   {
+    // Arrange
+    SetUpContext(gdpPrices: gdpPrices);
     var from = new DateTime(2020, 6, 1);
     var to = new DateTime(2022, 1, 1);
 
+    // Act
     var result = await sut.GetGDPPricesAsync(from, to);
 
+    // Assert
     Assert.That(result, Has.Count.EqualTo(2));
     Assert.That(result.First().Date, Is.EqualTo(new DateTime(2021, 1, 1)));
   }
@@ -51,11 +67,15 @@ public class FREDDataProviderTests
   [Test]
   public async Task GetW5000PricesAsync_ReturnsOrderedResults()
   {
+    // Arrange
+    SetUpContext(wilshirePrices: wilshirePrices);
     var from = new DateTime(2019, 1, 1);
     var to = new DateTime(2022, 12, 31);
 
+    // Act
     var result = await sut.GetW5000PricesAsync(from, to);
 
+    // Assert
     Assert.That(result, Has.Count.EqualTo(3));
     Assert.That(result, Is.Ordered.By("Date"));
   }
@@ -67,11 +87,15 @@ public class FREDDataProviderTests
   [Test]
   public async Task GetBuffettIndicatorAsync_CalculatesCorrectly()
   {
+    // Arrange
+    SetUpContext(gdpPrices: gdpPrices, wilshirePrices: wilshirePrices);
     var from = new DateTime(2020, 1, 1);
     var to = new DateTime(2022, 1, 1);
 
+    // Act
     var result = await sut.GetBuffettIndicatorAsync(from, to);
 
+    // Assert
     Assert.That(result, Has.Count.EqualTo(3));
 
     var sample = result.First();
@@ -82,42 +106,44 @@ public class FREDDataProviderTests
   [Test]
   public async Task GetBuffettIndicatorAsync_SkipsEntries_WhenGDPMissing()
   {
-    using (var scope = scopeFactory.CreateScope())
-    {
-      var db = scope.ServiceProvider.GetRequiredService<YieldDbContext>();
-      db.GDPPrices.RemoveRange(db.GDPPrices);
-      db.SaveChanges();
-    }
-
+    // Arrange
+    SetUpContext(wilshirePrices: wilshirePrices);
     var from = new DateTime(2020, 1, 1);
     var to = new DateTime(2022, 1, 1);
 
+    // Act
     var result = await sut.GetBuffettIndicatorAsync(from, to);
 
+    // Assert
     Assert.That(result, Is.Empty);
   }
 
   #endregion
 
-  private void SeedTestData()
+  #region HELPER
+
+  private void SetUpContext(
+      List<GDPPrice>? gdpPrices = null,
+      List<WilshirePrice>? wilshirePrices = null)
   {
     using var scope = scopeFactory.CreateScope();
     var db = scope.ServiceProvider.GetRequiredService<YieldDbContext>();
 
-    db.GDPPrices.AddRange(
-    [
-          new GDPPrice { Date = new DateTime(2020, 1, 1), Value = 20000 },
-          new GDPPrice { Date = new DateTime(2021, 1, 1), Value = 22000 },
-          new GDPPrice { Date = new DateTime(2022, 1, 1), Value = 25000 },
-      ]);
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
 
-    db.WilshirePrices.AddRange(
-    [
-          new WilshirePrice { Date = new DateTime(2020, 1, 1), Value = 24000 },
-          new WilshirePrice { Date = new DateTime(2021, 1, 1), Value = 27000 },
-          new WilshirePrice { Date = new DateTime(2022, 1, 1), Value = 31000 },
-      ]);
+    if (gdpPrices is { Count: > 0 })
+    {
+      db.GDPPrices.AddRange(gdpPrices);
+    }
+
+    if (wilshirePrices is { Count: > 0 })
+    {
+      db.WilshirePrices.AddRange(wilshirePrices);
+    }
 
     db.SaveChanges();
   }
+
+  #endregion
 }
